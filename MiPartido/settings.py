6236edeacb97a042525651e5f7b8291df5821477
AmIEviil <lokiinn@gmail.com>
2022-05-28 21:41:12 -0400

    master

--- MiPartido/settings.py
+++ MiPartido/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['C:/Users/lokii/OneDrive/Escritorio/MiPartidoDJANGO/MiPartido/MiPartido/core/static']
+#STATICFILES_DIRS = ['C:/Users/lokii/OneDrive/Escritorio/MiPartidoDJANGO/MiPartido/MiPartido/core/static']
 
+STATICFILES_DIRS = ['C:/Users/Gonzalo/Desktop/Nueva carpeta/MiPartidoDjango/core/static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
